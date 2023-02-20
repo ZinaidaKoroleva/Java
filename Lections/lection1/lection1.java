a6624f46e0554b8680519344dd382addd2256bdc
Zinaida <shymkova92@gmail.com>
2023-02-20 15:15:29 +0300

    Add lection1

---
+++ Lections/lection1/lection1.java
@@ -0,0 +1,51 @@
+package Lections.lection1;
+
+/**
+ * lection1
+ */
+public class lection1 {
+
+    public static void main(String[] args) {
+        // System.out.println("World"); // Распечатать в терминал
+        // String s = null;
+        // System.out.println(s);
+        // float f = 123.45f;
+        // char ch = 123;
+        // System.out.println(ch);
+        // var i = 123;
+        // // System.out.println(getType(i));
+        // System.out.println(i);
+        // var d = 123.6;
+        // System.out.println(d);
+        // int ik = 23_123_34;
+        // System.out.println(Integer.MIN_VALUE);
+        // String s = "qwer";
+        // s.charAt(1);
+        // System.out.println(s.charAt(1));
+        // int a = 123;
+        // a = --a-a--;
+        // System.out.print(a);
+        // boolean f = 123 > 122;
+        // System.out.print(f);
+        // int a = 8;
+        // 1000
+        // a = a << 1;
+        // System.out.println(a << 1 );
+        // int a = 5;
+        // int b = 2;
+        // System.out.println(a | b );
+        // 101
+        // 010
+        // 111
+        // int a = 5;
+        // int b = 2;
+        // System.out.println(a & b );
+        // 101
+        // 010
+        // 000
+        // int [] arr = new int [10];
+        // arr[3] = 13;
+        // System.out.print(arr[3]);
+    }
+
+}
