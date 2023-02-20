806dfeee69fedcad38e213a3058c824f39f7930f
Zinaida <shymkova92@gmail.com>
2023-02-20 15:18:24 +0300

    Add Homework2 Task1

---
+++ Homeworks/Homework2/Task1.java
@@ -0,0 +1,11 @@
+/** В файле содержится строка с исходными данными в такой форме:
+{"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
+Требуется на её основе построить и вывести на экран новую строку, в форме SQL запроса:
+SELECT * FROM students WHERE name = "Ivanov" AND country = "Russia" AND city = "Moscow";
+Для разбора строки используйте String.split. Сформируйте новую строку, используя StringBuilder. Значения null не включаются в запрос.
+ */
+package Homeworks.Homework2;
+
+public class Task1 {
+    
+}
