e4d5e0204c2a08c5b9d0c00c3eb63dfed0d266e7
Zinaida <shymkova92@gmail.com>
2023-02-21 18:03:27 +0300

    Try task2 HW2

---
+++ Homeworks/Homework2/Task2.java
@@ -0,0 +1,40 @@
+/**Реализуйте алгоритм сортировки пузырьком числового массива, 
+ * результат после каждой итерации запишите в лог-файл. */
+package Homeworks.Homework2;
+
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Task2 {
+    private static Logger logger = Logger.getLogger(Task2.class.getName());
+
+    public static void main(String[] args) {
+        try {
+            FileHandler fh = new FileHandler("/logging.txt");
+            logger.addHandler(fh);
+        } catch (IOException er) {
+            logger.log(Level.SEVERE, "Ошибка", er);
+        }
+        int[] array = { 1, 6, 2, 3, 9, 0, 4 };
+        System.out.println(Arrays.toString(array));
+        bubbleSort(array);
+    }
+
+    private static void bubbleSort(int[] array){
+
+        for (int ceiling = array.length; ceiling >=0; ceiling--) {
+            for (int i = 0; i < ceiling && i + 1 < ceiling; i++)
+                if (array[i]> array[i+1]) {
+                    int temp = array[i];
+                    array[i] = array[i+1];
+                    array[i+1] = temp;
+                    logger.log(Level.INFO, String.format("%d <-> %d, %s", array[i], array[i+1], Arrays.toString(array)));
+                }
+        }
+        System.out.println(Arrays.toString(array));
+    }
+}
