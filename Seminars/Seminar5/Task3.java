6a62aaa28b68d3ec48c9c0d57ebc24da02b000d6
Zinaida <qktrey@gmail.com>
2023-02-28 23:42:46 +0300

    Add Seminar 5 complite

--- Seminars/Seminar5/Task3.java
+++ Seminars/Seminar5/Task3.java
@@ -8,11 +8,39 @@
  */
 package Seminars.Seminar5;
 
+import java.util.ArrayDeque;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Task3 {
     public static void main(String[] args) {
-        
+        System.out.println(isValid("<{a}+{(d*3)}>"));
     }
+
     private static boolean isValid(String formula){
-        
+        Map<Character,Character> bracketsDict = new HashMap<>();
+        bracketsDict.put('(', ')');
+        bracketsDict.put('[', ']');
+        bracketsDict.put('{', '}');
+        bracketsDict.put('<', '>');
+
+        ArrayDeque<Character> brackets = new ArrayDeque<>();
+        for (char c : formula.toCharArray()) {
+            if (bracketsDict.containsKey(c)) // Если с является открывающей скобкой
+                brackets.add(c);
+
+            else if (bracketsDict.containsValue(c)){ // Если с закрывающая скобка
+
+                if (brackets.isEmpty())
+                    return false;
+                char openingBracket = brackets.removeLast();
+
+                if (bracketsDict.get(openingBracket) != c) { // Если закрывающая скобка не соответствует открывающей
+                    return false;
+                }
+            }
+                
+        }
+        return brackets.isEmpty();
     }
 }
